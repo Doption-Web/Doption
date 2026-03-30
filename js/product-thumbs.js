64051751c4f92ea0ebcc59bce60d447174f43d00
Doption-Web <doption-web-537@gmx.de>
2026-03-30 22:18:07 +0200

    Delete js directory

--- js/product-thumbs.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll('.product-card').forEach(card => {
-  const mainImg = card.querySelector('.main-img');
-  card.querySelectorAll('.thumb').forEach(thumb => {
-    thumb.addEventListener('click', () => {
-      mainImg.src = thumb.src;
-    });
-  });
-});
